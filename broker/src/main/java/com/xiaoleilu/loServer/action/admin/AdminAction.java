539e97d8b9344fbec51806a771b541c7e6843888
heavyrain.lee <heavyrain.lee@gmail.com>
2019-05-20 19:33:22 +0800

    server api时间戳检查开关

--- broker/src/main/java/com/xiaoleilu/loServer/action/admin/AdminAction.java
+++ broker/src/main/java/com/xiaoleilu/loServer/action/admin/AdminAction.java
@@ -26,8 +26,17 @@ import win.liyufan.im.RateLimiter;
 
 abstract public class AdminAction extends Action {
     private static String SECRET_KEY = "123456";
+    private static boolean NO_CHECK_TIME = false;
     public static void setSecretKey(String secretKey) {
         SECRET_KEY = secretKey;
+    }
+
+    public static void setNoCheckTime(String noCheckTime) {
+        try {
+            NO_CHECK_TIME = Boolean.parseBoolean(noCheckTime);
+        } catch (Exception e) {
+
+        }
     }
 
     @Override
@@ -50,8 +59,8 @@ abstract public class AdminAction extends Action {
             return ErrorCode.INVALID_PARAMETER;
         }
 
-        if (System.currentTimeMillis() - ts > 2 * 60 * 60 * 1000) {
-//            return ErrorCode.ERROR_CODE_SIGN_EXPIRED;
+        if (!NO_CHECK_TIME && System.currentTimeMillis() - ts > 2 * 60 * 60 * 1000) {
+            return ErrorCode.ERROR_CODE_SIGN_EXPIRED;
         }
 
         String str = nonce + "|" + SECRET_KEY + "|" + timestamp;
