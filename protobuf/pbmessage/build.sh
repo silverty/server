3442b5ab138b8f44e8eb94c47f749348158b239a
heavyrain lee <heavyrain.lee@gmail.com>
2019-06-18 08:25:00 +0800

    update script

--- protobuf/pbmessage/build.sh
+++ protobuf/pbmessage/build.sh
@@ -4,5 +4,5 @@ g++ -o convert convert.cpp
 mv pbdata.h ../../../proto/mars/proto/src/Proto/
 rm convert data.pb
 protoc --java_out=./ wfcmessage.proto
-mv ./cn/wildfirechat/proto/WFCMessage.java ../../client/src/main/java/cn/wildfirechat/proto/
+mv ./cn/wildfirechat/proto/WFCMessage.java ../../common/src/main/java/cn/wildfirechat/proto/
 rm -rf ./cn/wildfirechat/proto
